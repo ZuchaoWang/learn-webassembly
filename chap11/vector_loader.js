27fe4045e991b84bc18fac5067c00b9280297e57
ZuchaoWang <zuchao.wang@gmail.com>
2022-02-28 18:58:43 +0800

    all done

--- chap11/vector_loader.js
+++ chap11/vector_loader.js
@@ -2,17 +2,22 @@ const fs = require("fs");
 const loader = require("@assemblyscript/loader");
 (async () => {
   let wasm = fs.readFileSync("vector_loader.wasm");
-  // instantiate the module using the loader
+
   let module = await loader.instantiate(wasm);
-  // module.exports.Vector2D mirrors the AssemblyScript class.
-  let Vector2D = module.exports.Vector2D;
+  let { Vector2D, Vector3D } = module.exports;
+
   let vector1 = new Vector2D(3, 4);
   let vector2 = new Vector2D(4, 5);
+  let vector3 = new Vector3D(5, 6, 7);
+
   vector2.y += 10;
   vector2.add(vector1);
-  console.log(
-    `vector1=(${vector1.x}, ${vector1.y})\nvector2=(${vector2.x}, ${
-      vector2.y
-    })\nvector1.magnitude=${vector1.Magnitude()}\nvector2.magnitude=${vector2.Magnitude()}`
-  );
+  vector3.z++;
+
+  console.log(`vector1=(${vector1.x}, ${vector1.y})
+vector2=(${vector2.x}, ${vector2.y})
+vector3=(${vector3.x}, ${vector3.y}, ${vector3.z})
+vector1.magnitude=${vector1.Magnitude()}
+vector2.magnitude=${vector2.Magnitude()}
+vector3.magnitude=${vector3.Magnitude()}`);
 })();
--- chap11/vector_loader.ts
+++ chap11/vector_loader.ts
@@ -14,3 +14,19 @@ export class Vector2D {
     return this;
   }
 }
+
+export class Vector3D extends Vector2D {
+  z: f32;
+  constructor(x: f32, y: f32, z: f32) {
+    super(x, y);
+    this.z = z;
+  }
+  Magnitude(): f32 {
+    return Mathf.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+  }
+  add(vec3: Vector3D): Vector3D {
+    super.add(vec3);
+    this.z += vec3.z;
+    return this;
+  }
+}
